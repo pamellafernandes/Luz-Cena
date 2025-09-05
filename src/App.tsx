aab11b9fa4bb8d22e96b01d7b6973f97ab631060
pamellafernandes <pamellafernandes117@gmail.com>
2025-09-05 20:20:23 -0300

    feat: Added component Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Banner from "./components/Banner"
+import Link from "./components/Link"
 
 function App() {
   return (
+    <>
       <Banner src="./public/Banners/Banner.png" alt="Banner"/>
+      <Link href="/" target="_blank">Link</Link>
+    </>
   )
 }
 
